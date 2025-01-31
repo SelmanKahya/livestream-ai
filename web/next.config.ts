ac03e4ca3d7bd34f14cde97141f7e878fb6e9144
Selman K <selman@lindy.ai>
2025-02-01 00:19:33 +0300

    asd

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "build",
+  output: "standalone",
+  poweredByHeader: false,
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
